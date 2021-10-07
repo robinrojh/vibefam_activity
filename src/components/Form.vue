9c142c14c0f1be89a1a8b105dfef74b497e75298
Yong Gi <rygrobin@gmail.com>
2021-10-07 11:38:36 +0800

    setup with frontend

---
+++ src/components/Form.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="form">
+    <h1>{{ msg }}</h1>
+    <form @submit="onSubmit()">
+      Class Name: <input v-model="classname" required>
+      <button> Submit </button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      classname: ""
+    }
+  },
+  methods: {
+    onSubmit() {
+
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <Form msg="Enter a Class Name!"/>
+
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Form from '@/components/Form.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Form
+  }
+}
+</script>
