565b4f1ea821dc24061c8021fec1f5fb358c1f48
Yong Gi <rygrobin@gmail.com>
2021-10-07 22:54:33 +0800

    added input validation

--- functions/index.js
+++ functions/index.js
@@ -10,19 +10,30 @@ const app = express()
 
 app.use(cors({ origin: true }))
 
+/* 
+    Post method to post to classes collection and to update the metadata collection document.
+    Atomically updates both collections. 
+*/
 app.post('/', async (req, res) => {
     const className = req.body
-    await db.collection("classes").add({
-        className: className
-    })
+
+    const batch = await db.batch()
+    // Since batch write can only use set, update, and delete,
+    // I made classRef to be a doc with unique ID, then set it with req.body.
+    const classRef = db.collection("classes").doc()
+    const metadataRef = db.collection("metadata").doc("classes")
+    try {
+        await batch.set(classRef, className)
+        await batch.update(metadataRef, {
+            count: admin.firestore.FieldValue.increment(1)
+        })
+    } catch (error) {
+        console.error("batch write failed")
+        res.status(400).send()
+        return;
+    }
+    await batch.commit()
     res.status(201).send()
 })
 
 exports.classNameSubmit = functions.https.onRequest(app)
-exports.incrementCount = functions.firestore
-    .document("classes/{className}")
-    .onCreate(async (change, context) => {
-        await db.collection("metadata").doc("classes").update({
-            count: admin.firestore.FieldValue.increment(1)
-        })
-    })
