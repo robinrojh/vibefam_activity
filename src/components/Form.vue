87f125bb674edea1cfaa021bdd98aa665ec99b12
Yong Gi <rygrobin@gmail.com>
2021-10-07 23:06:24 +0800

    added form disable feature

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -2,31 +2,79 @@
   <div class="form">
     <h1>{{ msg }}</h1>
     <form @submit="onSubmit()">
-      Class Name: <input v-model="classname" required>
-      <button> Submit </button>
+      Class Name: <input v-model="className" required :disabled="disabled" />
+      <button>Submit</button>
+      <p>{{ errorMsg }}</p>
     </form>
   </div>
 </template>
 
 <script>
+import axios from "axios";
 export default {
-  name: 'HelloWorld',
+  name: "Form",
   props: {
-    msg: String
+    msg: String,
   },
   data() {
     return {
-      classname: ""
-    }
+      className: "",
+      errorMsg: "",
+      disabled: false
+    };
   },
   methods: {
+    /*
+    Limiting inputs with special characters, based on the link below.
+    https://firebase.google.com/docs/firestore/best-practices
+    Also, empty inputs are not allowed as the form is set to be required.
+    */
     onSubmit() {
-      if (this.classname != "") {
-        
+      const invalidArray = [".", "[", "]", "*", "`"];
+      let invalid = "";
+      let isValidated = true;
+      if (this.className.length == 0) {
+        isValidated = false;
       }
-    }
-  }
-}
+      for (let k = 0; k < this.className.length; k++) {
+        if (!isValidated) break;
+        for (let inv = 0; inv < invalidArray.length; inv++) {
+          if (this.className[k] == invalidArray[inv]) {
+            isValidated = false;
+            invalid = invalidArray[inv];
+            break;
+          }
+        }
+      }
+      if (isValidated) {
+        this.disabled = true
+        axios
+          .post(
+            "https://us-central1-vibefam-activity.cloudfunctions.net/classNameSubmit",
+            {
+              className: this.className,
+            }
+          )
+          .then((response) => {
+            this.className = ""
+            this.errorMsg = ""
+            this.disabled = false
+            console.log(response)
+          })
+          .catch((error) => {
+            this.disabled = false
+            console.error(error)
+          });
+      } else {
+        if (invalid !== "") {
+          this.errorMsg =
+            '"' + invalid + '"' + " is not a permitted character.";
+          console.error("Error! Your input contains invalid value: " + invalid);
+        }
+      }
+    },
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
